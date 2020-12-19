2a1555daa18ec75e033aa91c71fe87530d9ad01a
Tobias Staack <tobias-zunfix@web.de>
2020-12-19 18:55:38 +0100

    Update Server.cpp

--- Win-Net/Net/NetServer/Server.cpp
+++ Win-Net/Net/NetServer/Server.cpp
@@ -14,6 +14,10 @@ static void LatencyThread(Server::NET_PEER peer)
 	ICMP _icmp(ip.get());
 	_icmp.execute();
 
+	// since the above action is taking a while and freezing during this action, we want to check against peer validation again
+	if (!peer)
+		return;
+
 	peer->latency = _icmp.getLatency();
 	peer->bLatency = false;
 }
