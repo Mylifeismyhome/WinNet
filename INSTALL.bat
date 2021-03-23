1991580c97d1cf10656f490ad40650e3668deb07
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-03-23 17:00:55 +0100

    update

--- INSTALL.bat
+++ INSTALL.bat
@@ -9,13 +9,17 @@ echo -------------------------------------------
 REM TOOLS REQUIRED - INSTALL THEM FIRST
 REM --------------------------------------------------
 REM Microsoft Visual Studio Build Tools VC15
+echo [+] Downloading Visual Studio Build Tools
+curl https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwirkO7Q48bvAhXU8eAKHUMNAyAQFjAAegQIBBAD&url=https%3A%2F%2Fgo.microsoft.com%2Ffwlink%2F%3FLinkId%3D691126&usg=AOvVaw0geDw_h-TSCfzTMvYE2ZOw -O %CD%\Tools\visualcppbuildtools_full.exe
 echo [+] Installing Visual Studio Build Tools
 call %CD%\Tools\visualcppbuildtools_full.exe
-echo [+] Installing NASM
-call %CD%\Tools\nasm-2.14.02-installer-x64.exe
-echo [+] Downloading PERL
+echo [+] Downloading NASM (2.15.05)
+curl https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-installer-x64.exe -O nasm-2.15.05-installer-x64.exe
+echo [+] Installing NASM (2.15.05)
+call %CD%\Tools\nasm-2.15.05-installer-x64.exe
+echo [+] Downloading PERL (5.32.1.1)
 curl https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-64bit.msi -O %CD%\Tools\strawberry-perl-5.32.1.1-64bit.msi
-echo [+] Installing PERL
+echo [+] Installing PERL (5.32.1.1)
 call %CD%\Tools\strawberry-perl-5.32.1.1-64bit.msi
 REM --------------------------------------------------
 
