4d55470265b83bfe6dafc1f5dc17b5cf3da6c8f3
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-06-01 01:00:06 +0200

    Create socket.js

---
+++ Win-Net/Net/NetWebServer/socket.js
@@ -0,0 +1,105 @@
+"use strict";
+var Socket;
+
+class raw_t
+{
+	constructor(data, data_size)
+	{
+		this.data = data;
+		this.data_size = data_size;
+	}
+	
+	get src()
+	{
+		return this.data;
+	}
+	
+	get size()
+	{
+		return this.data_size;
+	}
+}
+
+function Connect(connect)
+{
+    Socket = new WebSocket(connect);
+	Socket.binaryType = "blob";
+    Socket.onopen = OnOpen;
+    Socket.onerror = OnClose;
+    Socket.onmessage = OnMessage;
+    Socket.onclose = OnClose;
+}
+
+function Disconnect() 
+{
+    Socket.close();
+}
+
+function OnOpen(e)
+{
+    OnOpenCallback();
+}
+
+function OnError(e)
+{
+	console.error("WebSocket error observed:", event);
+    Socket.close();
+    OnErrorCallback();
+}
+
+function OnMessage(e)
+{
+    Receive(e);
+}
+
+function OnClose(e)
+{
+    OnCloseCallback();
+}
+
+function Send(id, content) 
+{
+    var out = 
+    {
+        ID: parseInt(id),
+        CONTENT: content
+    };
+
+    var Json = JSON.stringify(out);
+    Socket.send(Json);
+}
+
+function Receive(event) 
+{
+	if(typeof event.data == 'object')
+	{
+		event.data.arrayBuffer().then(buffer => {
+			const view = new DataView(buffer);
+			const id = view.getUint32(0);
+			window["On" + GetEnumName(id)](new Uint8Array(buffer, 4));
+		});
+	}
+	else
+	{
+		var begin = event.data.indexOf("{");
+		var end = event.data.lastIndexOf("}") + 1;
+		var json = JSON.parse(event.data.substr(begin, end));
+		window["On" + GetEnumName(json.ID)](json.CONTENT);
+	}
+}
+
+function LOG(event) 
+{
+    console.log(event);
+}
+
+function GetEnumName(value) 
+{
+	for (var i in PKG)
+	{
+		if (PKG[i] == value) 
+		{
+			return i;
+		}
+	}
+}
