ce5ad742046897e9c2973f6343d4027613721304
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-06-14 20:52:42 +0200

    Update BUILD.bat

--- extern/OpenSSL/src/BUILD.bat
+++ extern/OpenSSL/src/BUILD.bat
@@ -1,8 +1,11 @@
+title COMPILING OPENSSL
+echo do not close me
+
 echo -------------------------------------------
 echo Start compiling OpenSSL :: %date% %time%
 echo -------------------------------------------
-start /WAIT INSTALL.bat
-start /WAIT TEST.bat
+call INSTALL.bat
+call TEST.bat
 start /WAIT BUILD_X64d_SHARED.bat
 start /WAIT BUILD_X64_SHARED.bat
 start /WAIT BUILD_X64d.bat
@@ -15,3 +18,5 @@ start /WAIT "COPY.bat"
 echo -------------------------------------------
 echo Done compiling OpenSSL :: %date% %time%
 echo -------------------------------------------
+
+exit
