af8afdd651b27e92b4babf215c5ecd8271bdf058
root <root@DESKTOP-PVC7J46.localdomain>
2021-07-25 17:07:51 +0200

    test linux

--- TestLinux/main.cpp
+++ TestLinux/main.cpp
@@ -1,9 +1,33 @@
 #include <Net/Net/Net.h>
 #include <Net/Net/NetString.h>
 #include <Net/Cryption/AES.h>
+#include <Net/Coding/BASE64.h>
 
 int main()
 {
+	LOG_ERROR(CSTRING("TEST %i"), 50);
+
+
+	const char* word = "test du huan";
+	size_t size = strlen(word);
+
+	BYTE* data = ALLOC<BYTE>(size + 1);
+	memcpy(data, word, size);
+	data[size] = '\0';
+
+	Net::Coding::Base64::encode(data, size);
+
+	printf("%s", data);
+
+
+	Net::Coding::Base64::decode(data, size);
+
+	printf("\n%s", data);
+
+	FREE(data);
+	return 0;
+	// not used
+
 	Net::String str("Test");
 	str.append(" ES FUNKT!");
 	printf(str.get().get());
@@ -11,8 +35,6 @@ int main()
 	NET_AES aes;
 	aes.Init((char*)"TEST", (char*)"HUAN");
 
-	const char* word = "test du huan";
-	size_t size = strlen(word);
 	BYTE* enc = ALLOC<BYTE>(size + 1);
 	memcpy(enc, word, size);
 	enc[size] = '\0';
