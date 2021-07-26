831616105fd9ad92c7add93cc7cf52bcf3628150
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-07-26 23:13:36 +0200

    fixed windows support

--- SET_CONFIG.bat
+++ SET_CONFIG.bat
@@ -1,20 +1,16 @@
 @echo off
-openfiles >NUL 2>&1 
-if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
- 
+
+if not exist "Config" mkdir Config
+
 set /p TOOLS_PATH=[+] Enter Microsoft Visual Studio Build Tools Path: 
 echo %TOOLS_PATH%>%CD%\Config\BUILDTOOLS_PATH
 
 REM Add PERL to Environment Scope
 set /p PERL_PATH=[+] Enter PERL Path: 
-setx /M PATH "%PATH%;%PERL_PATH%"
+setx /s PATH "%PATH%;%PERL_PATH%"
 
 REM Add NASM to Environment Scope
 set /p NASM_PATH=[+] Enter NASM Path: 
-setx /M PATH "%PATH%;%NASM_PATH%"
+setx /s PATH "%PATH%;%NASM_PATH%"
 
-goto End
-:NotAdmin 
-echo This command prompt is NOT ELEVATED (NOT Run as administrator). 
-:End
 pause
