93e3215b0d115cf482bd2925a0432fff6b060fbe
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-08-06 00:16:57 +0200

    Update copy.bat

--- Win-Net/Net/copy.bat
+++ Win-Net/Net/copy.bat
@@ -3,7 +3,17 @@ echo ---------- NET XCOPY INCLUDE FILES ----------
 xcopy "..\..\..\extern\crypto++\include\" "..\..\extern\Net\include\" /s /i /q /y /c
 xcopy "..\..\..\extern\JSON\" "..\..\extern\Net\include\JSON\" /s /i /q /y /c
 xcopy "..\..\..\extern\MYSQL\include\" "..\..\extern\Net\include\MYSQL\" /s /i /q /y /c
-xcopy "..\..\..\extern\OpenSSL\include\openssl\*.h" "..\..\extern\Net\include\OpenSSL\" /s /i /q /y /c
+
+REM Copy from any source available, all include headers are the same
+xcopy "..\..\..\extern\OpenSSL\bin\dll\x64\release\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\dll\x64\debug\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\dll\x86\release\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\dll\x86\debug\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\lib\x64\release\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\lib\x86\release\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\lib\x64\debug\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+xcopy "..\..\..\extern\OpenSSL\bin\lib\x86\debug\include" "..\..\extern\Net\include\" /s /i /q /y /c /d
+
 xcopy "..\..\..\extern\pugiXML\src\*.hpp" "..\..\extern\Net\include\pugiXML\" /s /i /q /y /c
 xcopy "..\..\..\extern\ZLib\*.h" "..\..\extern\Net\include\ZLib\" /s /i /q /y /c
 xcopy "..\..\..\extern\BCrypt\" "..\..\extern\Net\include\BCrypt\" /s /i /q /y /c
