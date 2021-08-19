8fe0e6effc548d08daaf1de8407e3d1d43c515f3
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-08-19 13:59:12 +0200

    Update NetBuildConfig.h

--- Win-Net/Net/Net/Net/NetBuildConfig.h
+++ Win-Net/Net/Net/Net/NetBuildConfig.h
@@ -1,25 +1,13 @@
 #pragma once
 
-// need to implement
-/*
-mysqlcppconn.lib
-mysqlcppconn-static.lib
-*/
-
+/* define to use MYSQL c++ connector */
 #define NET_USE_MYSQL_SUPPORT
-#define BUILD_LINUX
 
-/*
-	define this to build for visual studio 2013
-*/
+/* define to build for linux */
+#undef BUILD_LINUX
+
+/* define this to build for visual studio 2013 */
 #undef VS13
 
+/* define to track memory */
 #undef NET_TEST_MEMORY_LEAKS
-
-// disable windows compitable code
-#ifdef BUILD_LINUX
-#define NET_DISABLE_MEMORY_IMPORT_DLL
-#define NET_DISABLE_IMPORT_KERNEL32
-#define NET_DISABLE_IMPORT_NTDLL
-#define NET_DISABLE_IMPORT_WS2_32
-#endif
