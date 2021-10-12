8ea5bcf1045236549d04e567b28f1068261d767a
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-10-12 15:42:18 +0200

    Update NetBuildConfig.h

--- Win-Net/Net/Net/Net/NetBuildConfig.h
+++ Win-Net/Net/Net/Net/NetBuildConfig.h
@@ -14,7 +14,7 @@
 
 /* define to disable logging */
 #undef NET_CORE__DISABLE_LOGMANAGER /* enable to disable logmanager for core only */
-#define NET_CLIENT__DISABLE_LOGMANAGER /* enable to disable logmanager for client only */
+#undef NET_CLIENT__DISABLE_LOGMANAGER /* enable to disable logmanager for client only */
 #undef NET_SERVER__DISABLE_LOGMANAGER /* enable to disable logmanager for server only */
 #undef NET_WEBSERVER__DISABLE_LOGMANAGER /* enable to disable logmanager for webserver only */
 #undef NET_DISABLE_LOGMANAGER /* enable to disable entire logmanager for every porject */
