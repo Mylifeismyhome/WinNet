00ace5df03c08a27905f40ec02be3c37447b2465
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-12-09 19:08:59 +0100

    config to exclude mysql from compiling

--- Win-Net/Net/Net/Net/NetBuild.h
+++ Win-Net/Net/Net/Net/NetBuild.h
@@ -18,6 +18,10 @@
 #define NET_DISABLE_IMPORT_User32
 #endif
 
+#ifdef NET_EXCLUDE_MYSQL_SUPPORT
+#undef NET_USE_MYSQL_SUPPORT
+#endif
+
 /* disable dynamic link */
 #ifdef NET_USE_MYSQL_SUPPORT
 #define CPPCONN_LIB_BUILD
