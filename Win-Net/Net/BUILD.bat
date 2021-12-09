27fa931e25584902d932857312eaf6d20a7afbd4
Mylifeismyhome <43041558+Mylifeismyhome@users.noreply.github.com>
2021-12-09 22:20:45 +0100

    Update BUILD.bat

--- Win-Net/Net/BUILD.bat
+++ Win-Net/Net/BUILD.bat
@@ -5,28 +5,43 @@ title Compile WinNet
 REM Select Mode
 echo Mode:
 echo 	1. DEBUG_DLL
-echo		2. DEBUG_LIB_MDD
-echo		3. DEBUG_LIB_MTD
-echo		4. RELEASE_DLL
-echo		5. RELEASE_LIB_MD
-echo		6. RELEASE_LIB_MT
-echo		7. RELEASE_LIB_MT_VS13
+echo		2. DEBUG_DLL_EXCLUDE_MYSQL
+echo		3. DEBUG_LIB_MDD
+echo		4. DEBUG_LIB_MDD_EXCLUDE_MYSQL
+echo		5. DEBUG_LIB_MTD
+echo		6. DEBUG_LIB_MTD_EXCLUDE_MYSQL
+echo		7. RELEASE_DLL
+echo		8. RELEASE_DLL_EXCLUDE_MYSQL
+echo		9. RELEASE_LIB_MD
+echo		10. RELEASE_LIB_MD_EXCLUDE_MYSQL
+echo		11. RELEASE_LIB_MT
+echo		12. RELEASE_LIB_MT_EXCLUDE_MYSQL
 echo ---------------------------
 set /p MODE=Select Mode: 
 if %MODE%==1 (
 	set MODE=DEBUG_DLL
 ) else if %MODE%==2 (
-	set MODE=DEBUG_LIB_MDD
+	set MODE=DEBUG_DLL_EXCLUDE_MYSQL
 ) else if %MODE%==3 (
-	set MODE=DEBUG_LIB_MTD
+	set MODE=DEBUG_LIB_MDD
 ) else if %MODE%==4 (
-	set MODE=RELEASE_DLL
+	set MODE=DEBUG_LIB_MDD_EXCLUDE_MYSQL
 ) else if %MODE%==5 (
-	set MODE=RELEASE_LIB_MD
+	set MODE=DEBUG_LIB_MTD
 ) else if %MODE%==6 (
-	set MODE=RELEASE_LIB_MT
+	set MODE=DEBUG_LIB_MTD_EXCLUDE_MYSQL
 ) else if %MODE%==7 (
-	set MODE=RELEASE_LIB_MT_VS13
+	set MODE=RELEASE_DLL
+) else if %MODE%==8 (
+	set MODE=RELEASE_DLL_EXCLUDE_MYSQL
+) else if %MODE%==9 (
+	set MODE=RELEASE_LIB_MD
+) else if %MODE%==10 (
+	set MODE=RELEASE_LIB_MD_EXCLUDE_MYSQL
+) else if %MODE%==11 (
+	set MODE=RELEASE_LIB_MT
+) else if %MODE%==12 (
+	set MODE=RELEASE_LIB_MT_EXCLUDE_MYSQL
 ) else (
 	echo Invalid MODE
 	pause
