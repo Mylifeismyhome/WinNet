2779a959b0bc1ba352c986cdd3ffdc90560c60a2
Mylifeismyhome <Tobias-zunfix@web.de>
2022-09-09 03:11:49 +0200

    undefined macro

--- Sandbox/Client/Client/main.cpp
+++ Sandbox/Client/Client/main.cpp
@@ -20,15 +20,15 @@ int main()
 	doc["Test"] = 5;
 	doc["ABC"]["DCE"] = 25.12f;
 
-	//Net::Json::Array arr;
-	//arr.push(25);
-	//arr.push("TEST");
-	//arr.push(13.125f);
+	Net::Json::Array arr;
+	arr.push(25);
+	arr.push("TEST");
+	arr.push(13.125f);
 
-	//doc["ARR"] = arr;
+	doc["ARR"] = arr;
 
-	/*std::cout << doc["ARR"]->as_array()->operator[](1)->as_string() << std::endl;
-	std::cout << doc["ARR"]->as_array()->at(1)->as_string() << std::endl;*/
+	std::cout << doc["ARR"]->as_array()->operator[](1)->as_string() << std::endl;
+	std::cout << doc["ARR"]->as_array()->at(1)->as_string() << std::endl;
 
 	std::cout << doc.Serialize(Net::Json::SerializeType::NONE).get().get() << std::endl;
 
